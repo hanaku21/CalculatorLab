1133a5ef3fe058e65639dccda13b2b42928fad9e
hanaku21 <fon_uta_21@hotmail.com>
2017-09-05 16:07:41 +0700

    add rpncalengine

---
+++ CPE200Lab1/CPE200Lab1/RpnCalculatorEngine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace CPE200Lab1
+{
+    class RpnCalculatorEngine : CalculatorEngine
+    {
+        private void testStackMethod()
+        {
+            Stack testStack = new Stack();
+            for (int i =1; i <3; i++)
+            {
+                testStack.Push(i.ToString() + " element");
+                
+            }
+            
+        }
+    }
+}
